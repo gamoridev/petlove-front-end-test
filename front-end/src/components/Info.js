7e824070612af60db81117c529f0847d2aa3ba18
gamoridev <gamoridev@protonmail.com>
2019-05-18 16:28:57 -0300

    Order result and format output

--- front-end/src/components/Info.js
+++ front-end/src/components/Info.js
@@ -2,14 +2,23 @@ import React from 'react';
 import './Info.css';
 
 function Info({ data }) {
+    let orderedObject = {};
+    orderedObject.cep = { value: data.cep.replace(/-/g, ""), label: 'CEP', order: 0 }
+    orderedObject.uf = { value: data.uf, label: 'Estado', order: 1 }
+    orderedObject.localidade = { value: data.localidade, label: 'Cidade', order: 2 }
+    orderedObject.logradouro = { value: data.logradouro, label: 'Logradouro', order: 3 }
+
     return (
         <dl className="Info__list">
-            {Object.keys(data).map((e, k) => (
-            	<React.Fragment key={k}>
-	                <dt className="Info__term">{e}:</dt>
-	                <dd className="Info__description">{data[e]}</dd>
-            	</React.Fragment>
-            ))}
+            { Object.keys(orderedObject).sort((a, b) => (orderedObject[a].order > orderedObject[b].order) ? 1 : -1)
+                .map((e, k) => {
+                    return (
+                    	<React.Fragment key={k}>
+        	                <dt className="Info__term">{orderedObject[e].label}:</dt>
+        	                <dd className="Info__description">{orderedObject[e].value}</dd>
+                    	</React.Fragment>
+                    )   
+            })}
         </dl>
     );
 }
